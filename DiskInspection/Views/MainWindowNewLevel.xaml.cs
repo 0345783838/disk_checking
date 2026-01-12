cf29f14b07dc75199706f38ca48ea47ced404456
Huynh Vi <linconvi98@gmail.com>
2026-01-12 23:41:42 +0700

    add MainWindow new

---
+++ DiskInspection/Views/MainWindowNewLevel.xaml.cs
@@ -0,0 +1,251 @@
+﻿using DiskInspection.Controllers;
+using DiskInspection.Models;
+using DiskInspection.Views;
+using DiskInspection.Views.DebugWindows;
+using DiskInspection.Views.SettingsWindows;
+using DiskInspection.Views.UtilitiesWindows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiskInspection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowNewLevel : Window, INotifyPropertyChanged
+    {
+        private readonly static NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private MainController _mainController;
+        public int AiStatus { get; set; } = (int)(StatusState.UNKNOWN);
+        public int PlcStatus { get; set; } = (int)(StatusState.UNKNOWN);
+        public int Cam1Status { get; set; } = (int)(StatusState.UNKNOWN);
+        public int Cam2Status { get; set; } = (int)(StatusState.UNKNOWN);
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainController = new MainController(this);
+            DataContext = this;
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new CommonSettingsWindow();
+            window.Show();
+        }
+
+        private void btnDebug_Click(object sender, RoutedEventArgs e)
+        {
+            var debugWindow = new DebugWindow();
+            debugWindow.Show();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            WaitingWindow wait = new WaitingWindow("Checking running conditions...\rKiểm tra điều kiện chạy");
+            new Task(() =>
+            {
+                _mainController.Start();
+                wait.KillMe = true;
+            }).Start();
+            wait.ShowDialog();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            new Task(new Action(() =>
+            {
+                var res = _mainController.RunServiceAsync(20000, "Program is loading...");
+            })).Start();
+        }
+
+        internal void SetLoadingService(string content)
+        {
+            var timeout = 10000;
+            new Thread(() =>
+            {
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    WaitingWindow wait = new WaitingWindow(content);
+                    new Task(() =>
+                    {
+                        var timestep = timeout / 500;
+                        for (int i = 0; i < timestep; i++)
+                        {
+                            Thread.Sleep(500);
+                            if (_mainController._serviceIsRun)
+                            {
+                                break;
+                            }
+                        }
+                        wait.KillMe = true;
+                        UpdateAIStatus(true);
+                        if (!_mainController._serviceIsRun)
+                        {
+                            this.Dispatcher.Invoke(new Action(() =>
+                            {
+                                UpdateAIStatus(false);
+                                var box = new ErrorWindow("Cannot start AI service! Please contact IT!\rKhông khởi động được AI, Hãy liên hệ bộ phận PI");
+                                box.ShowDialog();
+                            }));
+                        }
+                    }).Start();
+                    wait.ShowDialog();
+                }));
+            }).Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _mainController.CloseCamera();
+            _mainController.CloseAIService();
+            foreach (var item in System.Windows.Application.Current.Windows)
+            {
+                if (item != this)
+                {
+                    ((Window)item).Close();
+                }
+            }
+            Environment.Exit(0);
+        }
+        #region Show Dialogs
+        public bool ShowWarning(string content)
+        {
+            var res = false;
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                var box = new WarningWindow(content);
+                box.ShowDialog();
+                res = (bool)box.DialogResult;
+            }));
+            return res;
+        }
+        public void ShowError(string content)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                var box = new ErrorWindow(content);
+                box.ShowDialog();
+            }));
+        }
+        public void ShowInfo(string content)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                var box = new InformationWindow(content);
+                box.ShowDialog();
+            }));
+        }
+
+        private void UpdateAIStatus(bool resAI)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                AiStatus = resAI ? (int)(StatusState.OK) : (int)(StatusState.NG);
+                OnPropertyChanged(nameof(AiStatus));
+            }));
+        }
+        internal void SetStatusService(bool resAI, bool resPLC, bool resCamera1, bool resCamera2)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                AiStatus = resAI ? (int)(StatusState.OK) : (int)(StatusState.NG);
+                PlcStatus = resPLC? (int)(StatusState.OK) : (int)(StatusState.NG);
+                Cam1Status = resCamera1? (int)(StatusState.OK) : (int)(StatusState.NG);
+                Cam2Status = resCamera2? (int)(StatusState.OK) : (int)(StatusState.NG);
+
+                OnPropertyChanged(nameof(AiStatus));
+                OnPropertyChanged(nameof(PlcStatus));
+                OnPropertyChanged(nameof(Cam1Status));
+                OnPropertyChanged(nameof(Cam2Status));
+            }));
+        }
+        #endregion
+
+        #region Update Image
+
+
+        public void UpdateCam1WhiteOrigin(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam1WhiteOrigin.Source = image;
+            });
+        }
+        public void UpdateCam1WhiteResult(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam1WhiteResult.Source = image;
+            });
+        }
+        public void UpdateCam1UvResult(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam1UvResult.Source = image;
+            });
+        }
+        public void UpdateCam1UvOrigin(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam1UvOrigin.Source = image;
+            });
+        }
+        public void UpdateCam2WhiteOrigin(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam2WhiteOrigin.Source = image;
+            });
+        }
+        public void UpdateCam2WhiteResult(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam2WhiteResult.Source = image;
+            });
+        }
+        public void UpdateCam2UvResult(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam2UvResult.Source = image;
+            });
+        }
+        public void UpdateCam2UvOrigin(BitmapSource image)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                imbCam2UvOrigin.Source = image;
+            });
+        }
+
+        #endregion
+    }
+}
